2608a158f9df03bd1f50cc8a5ee7e9cbcb63d18e
AleixMarin <aleix.marin@talenttools.net>
2024-01-09 17:31:15 +0100

    Iteration#3

--- JS/basicAlgorithms/app.js
+++ JS/basicAlgorithms/app.js
@@ -88,4 +88,59 @@ car2.finalPrice = car2.basePrice + globalBasePrice;
 console.log (car2.finalPrice);*/
 
 
+/*
+Iteración #3: Operadores
+1.1 Multiplica 10 por 5 y muestra el resultado mediante console.
 
+1.2 Divide 10 por 2 y muestra el resultado en un console.
+
+1.3 Muestra mediante un console el resto de dividir 15 por 9.
+
+1.4 Usa el correcto operador de asignación que resultará en o = 15, 
+teniendo dos variables p = 10 y j = 5.
+
+1.5 Usa el correcto operador de asignación que resultará en i = 50,
+teniendo dos variables c = 10 y m = 5.
+*/
+
+
+//1.1 ---------------------------------------------
+
+let num1 = 10;
+let num2 = 5;
+
+let multiplicacion = num1 * num2;
+
+console.log (multiplicacion);
+
+//1.2 ---------------------------------------------
+
+let num3 = 2;
+
+let division = num1 / num3;
+
+console.log (division);
+
+//1.3 ---------------------------------------------
+
+let a = 15;
+let b = 9;
+
+let resto = a % b;
+console.log (resto);
+
+//1.4 ---------------------------------------------
+
+let p = 10;
+let j = 5;
+
+let o = p + j;
+//console.log (o);
+
+//1.5 ---------------------------------------------
+
+let c = 10;
+let m = 5;
+
+let i = c * m;
+//console.log (i);
