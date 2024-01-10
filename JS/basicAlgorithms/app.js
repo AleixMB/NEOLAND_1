f57a9aa390c29e9150676f3879b45c541f956219
AleixMarin <aleix.marin@talenttools.net>
2024-01-10 16:08:05 +0100

    Iteration#4

--- JS/basicAlgorithms/app.js
+++ JS/basicAlgorithms/app.js
@@ -144,3 +144,72 @@ let m = 5;
 
 let i = c * m;
 //console.log (i);
+
+
+/*
+Iteración #4: Arrays
+1.1 Consigue el valor "HULK" del array de avengers y muestralo por consola.
+const avengers = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
+
+1.2 Cambia el primer elemento de avengers a "IRONMAN"
+const avengers = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
+
+1.3 console numero de elementos en el array usando la propiedad correcta de Array.
+const avengers = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
+
+1.4 Añade 2 elementos al array: "Morty" y "Summer". 
+Muestra en consola el último personaje del array
+const rickAndMortyCharacters = ["Rick", "Beth", "Jerry"];
+
+1.5 Elimina el último elemento del array y muestra el primero y el último por consola.
+const rickAndMortyCharacters = ["Rick", "Beth", "Jerry", "Morty", "Summer", "Lapiz Lopez"];
+
+1.6 Elimina el segundo elemento del array y muestra el array por consola.
+const rickAndMortyCharacters = ["Rick", "Beth", "Jerry", "Morty", "Summer", "Lapiz Lopez"];
+*/
+
+//1.1 ---------------------------------------------
+
+const avengers = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
+
+let primero = avengers[0];
+//console.log(primero);
+
+//1.2 ---------------------------------------------
+
+avengers[0] = "IRONMAN";
+//console.log (avengers);
+
+//1.3 ---------------------------------------------
+
+console.log (avengers.length);
+
+//1.4 --------------------------------------------
+
+const rickAndMortyCharacters = ["Rick", "Beth", "Jerry"];
+
+rickAndMortyCharacters.push ('Morty');
+rickAndMortyCharacters.push ('Summer');
+//console.log (rickAndMortyCharacters);
+
+let ultimo = rickAndMortyCharacters[rickAndMortyCharacters.length-1];
+console.log (ultimo);
+
+//1.5 --------------------------------------------
+
+const rickAndMortyCharacters2 = ["Rick", "Beth", "Jerry", "Morty", "Summer", "Lapiz Lopez"];
+
+rickAndMortyCharacters2.pop();
+//console.log (rickAndMortyCharacters2);
+
+let uno = rickAndMortyCharacters2[0]
+let last = rickAndMortyCharacters2[rickAndMortyCharacters2.length-1];
+console.log (uno , last);
+
+//1.6 --------------------------------------------
+
+const rickAndMortyCharacters3 = ["Rick", "Beth", "Jerry", "Morty", "Summer", "Lapiz Lopez"];
+
+let elim2 = rickAndMortyCharacters3.splice(1, 1);
+//console.log (elim2);
+console.log (rickAndMortyCharacters3);
