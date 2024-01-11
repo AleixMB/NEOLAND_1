6c213e098e98a502f491ea78c3d680405bf13939
AleixMarin <aleix.marin@talenttools.net>
2024-01-11 15:51:38 +0100

    Iteration#1

---
+++ JS/functions/app.js
@@ -0,0 +1,16 @@
+/***Iteración #1: Buscar el máximo**
+
+Completa la función que tomando dos números como argumento devuelva el más alto.
+
+```jsx
+function sum(numberOne , numberTwo) {
+  // insert code
+}
+```*/
+
+function sum(numberOne , numberTwo) {
+    console.log(Math.max(numberOne, numberTwo));
+};
+
+sum(10, 15);
+
