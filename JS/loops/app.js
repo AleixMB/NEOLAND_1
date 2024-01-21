0d26b1d4f02736c28567e6a37c09fdb17742bc77
AleixMarin <aleix.marin@talenttools.net>
2024-01-21 20:11:45 +0100

    End loops and Iteration#1

--- JS/loops/app.js
+++ JS/loops/app.js
@@ -132,3 +132,37 @@ const toys = [
     }
   }
   console.log (juguete);
+
+
+  /**Iteración #7: For...of avanzado**
+
+Usa un bucle **for...of** para recorrer todos los juguetes y añade los que tengan más de 15 ventas (sellCount) al array popularToys. Imprimelo por consola.. Puedes usar este array:
+
+```jsx
+const popularToys = [];
+const toys = [
+	{id: 5, name: 'Buzz MyYear', sellCount: 10}, 
+	{id: 11, name: 'Action Woman', sellCount: 24}, 
+	{id: 23, name: 'Barbie Man', sellCount: 15}, 
+	{id: 40, name: 'El gato con Guantes', sellCount: 8},
+	{id: 40, name: 'El gato felix', sellCount: 35}
+]
+```*/
+
+const toys1 = [
+	{id: 5, name: 'Buzz MyYear', sellCount: 10}, 
+	{id: 11, name: 'Action Woman', sellCount: 24}, 
+	{id: 23, name: 'Barbie Man', sellCount: 15}, 
+	{id: 40, name: 'El gato con Guantes', sellCount: 8},
+	{id: 40, name: 'El gato felix', sellCount: 35}
+]
+
+const bestToys = [];
+
+for (let toys2 of toys1) {
+  if (toys2.sellCount > 15) {
+    bestToys.push(toys2); 
+  }
+}
+console.log(bestToys);
+
