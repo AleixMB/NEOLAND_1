1570d85bc36da54b96bfd9f1f6ccb9b9ba6859b4
AleixMarin <aleix.marin@talenttools.net>
2024-01-31 17:06:18 +0100

    NODE1

---
+++ NODE/index.js
@@ -0,0 +1,16 @@
+//? CREAMO NUESTRO SERVIDOR EXPRESS
+
+//PARA LLAMAR A LAS VARIABLES DE RETORNO .ENV
+
+const express = require("express");
+const dotenv = require("dotenv");
+const { connect } = require("./src/utils/db");
+
+// creamos el servidor web
+const app = express();
+
+// vamos a configurar dotenv para poder utilizar las variables d entorno del .env
+dotenv.config();
+
+//! conectamos con la base de datos
+connect();
