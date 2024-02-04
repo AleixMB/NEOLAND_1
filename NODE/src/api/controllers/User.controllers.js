dba3c4e5a3eb54ea0cd103c6728b7b914458be8c
AleixMarin <aleix.marin@talenttools.net>
2024-02-05 00:10:43 +0100

    Login2

--- NODE/src/api/controllers/User.controllers.js
+++ NODE/src/api/controllers/User.controllers.js
@@ -260,5 +260,27 @@ const sendCode = async (req, res, next) => {
     return next(error);
   }
 };
+
+const login = async (req, res, next) => {
+  try { 
+    const { email, password } = req.body;
+    const userDB = await User.findOne({ email });
+    if (userDB) {
+      if (bcrypt.compareSync(password, userDB.password)) {
+        const token = generateToken(userDB._id, email);
+        return res.status(200).json({
+          user: userDB,
+          token,
+        });
+      } else {
+        return res.status(404).json('password dont match');
+      }
+    } else {
+      return res.status(404).json('User no register');
+    }
+  } catch (error) {
+    return next(error);
+  }
+};
   
-module.exports = { registerLargo, register, sendCode, registerWithRedirect };
+module.exports = { registerLargo, register, sendCode, registerWithRedirect, login };
